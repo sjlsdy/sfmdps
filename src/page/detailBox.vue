6807454aa61770d0ad4c64402b1edeb61ba9b3cb
songjunlong <songjunlong@air-x.com.cn>
2017-11-22 22:47:01 +0800

    ...

--- src/page/detailBox.vue
+++ src/page/detailBox.vue
@@ -1,10 +1,14 @@
 <template>
-	<div :style="{left:boxLeft}" class="box">
-		<div class="close-btn">
-			<Button type="primary" shape="circle" icon="close-round" @click="closeBtn"></Button>
-		</div>
-		<div class="boxmain">
-			<issuesDetail ref="issuesDetail"></issuesDetail>
+	<div>
+		<div class="boxBg" v-if="boxBgView"></div>
+		{{bodyWidth}}
+		<div :style="{left:boxLeft}" class="box">
+			<div class="close-btn">
+				<Button type="primary" shape="circle" icon="close-round" @click="closeBtn"></Button>
+			</div>
+			<div class="boxmain">
+				<issuesDetail ref="issuesDetail"></issuesDetail>
+			</div>
 		</div>
 	</div>
 </template>
@@ -17,30 +21,36 @@
 		},
 		data() {
 			return {
+				boxBgView: false,
 				bodyWidth: 0,
 				boxLeft: '100%',
 			}
 		},
 		methods: {
-			showPage(id) {
-				this.$refs.issuesDetail.initData(id);
+			initSize() {
 				this.bodyWidth = document.body.clientWidth;
+			},
+			showPage(id) {
+				this.initSize();
+				this.$refs.issuesDetail.initData(id);
 				this.boxLeft = this.bodyWidth + 'px';
 				this.setOpen(this.bodyWidth, 1);
+				this.boxBgView = true;
 			},
 			closeBtn() {
-				this.$emit("closeDetailBox")
-				this.setClose(0, 1);
+				this.initSize();
+				this.boxLeft = this.bodyWidth / 2 + 'px';
+				this.setClose(this.bodyWidth / 2, 1);
 			},
 			setOpen(num, level) {
 				var _slef = this;
-				if(num > 0) {
+				if(num > _slef.bodyWidth / 2) {
 					_slef.boxLeft = num + 'px';
 					setTimeout(function() {
 						_slef.setOpen(num - (20 * (level * 0.2)), level + 1);
 					}, 10);
 				} else {
-					this.boxLeft = '0px';
+					_slef.boxLeft = '50%';
 				}
 
 			},
@@ -52,7 +62,9 @@
 						_slef.setClose(num + (20 * (level * 0.2)), level + 1);
 					}, 10);
 				} else {
-					this.boxLeft = '100%';
+					_slef.boxLeft = '100%';
+					_slef.boxBgView = false;
+					_slef.$emit("closeDetailBox")
 				}
 			},
 		}
@@ -63,19 +75,36 @@
 	.box {
 		overflow: hidden;
 		position: fixed;
-		width: 100%;
+		width: 50%;
 		top: 0;
 		bottom: 0;
-		background: rgba(255, 255, 255, 0.95);
-		box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+		background: none;
+		box-shadow: 0 0 20px rgba(0, 0, 0, 0.25);
 		z-index: 999;
 	}
+	
 	.box .close-btn {
 		position: absolute;
 		top: 20px;
-		left: 20px;
+		right: 20px;
 	}
+	
 	.box .boxmain {
+		width: 100%;
+		height: 100%;
 		overflow-y: auto;
+		background: #FFF;
+		overflow-y: auto;
+		box-shadow: 0 0 10px rgba(0, 0, 0, 0.25);
+	}
+	
+	.boxBg {
+		background: rgba(0, 0, 0, 0.15);
+		position: fixed;
+		z-index: 998;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
 	}
 </style>
